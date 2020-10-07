aa3778e7159625f9f1d8a329bb228cd1cc7e0f4a
Gangbaekho <roorooro9933@gmail.com>
2020-10-07 18:53:49 +0900

    playing with sprite properties

--- src/Animation.js
+++ src/Animation.js
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Spritesheet from 'react-responsive-spritesheet'
+import {Grid} from '@material-ui/core'
 
 const Animation = (props) => {
+    
+    const [direction,setDirection] = useState('true')
 
     return (
+        <Grid container justify="center">
+        <Grid item xs={6}>
         <Spritesheet
         className={`my-element__class--style`}
         image={`https://raw.githubusercontent.com/danilosetra/react-responsive-spritesheet/master/assets/images/examples/sprite-image-horizontal.png`}
         widthFrame={420}
         heightFrame={500}
+        width={42}
+        height={500}
         steps={14}
         fps={10}
         autoplay={true}
+        isResponsive={true}
         loop={true}
+        onClick={(spritesheet)=>{
+            console.log(direction)
+            setDirection(!direction)
+            direction ? spritesheet.setDirection('forward') : spritesheet.setDirection('rewind')
+        }}
+        onDoubleClick={()=>{
+            alert('double clicked..')
+        }}
+        onMouseEnter={(spritesheet)=>{
+            console.log('onMouseEnter')
+        }}
+        onMouseOver={(spritesheet)=>{
+            console.log('onMouseOver')
+        }}
+        background={"static/images/village.jpg"}
+        backgroudSize="cover"
       />
+      </Grid>
+      </Grid>
     )
 }
 
