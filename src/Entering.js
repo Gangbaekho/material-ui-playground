b941bf0ed81b270c0723b66b998db401389ad4dc
Gangbaekho <roorooro9933@gmail.com>
2020-10-07 17:50:49 +0900

    add media card to component

--- src/Entering.js
+++ src/Entering.js
@@ -1,12 +1,63 @@
 import React from 'react'
 import Navigation from './Navigation'
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/styles'
+import {Grid} from '@material-ui/core'
+
+const useStyles = makeStyles({
+    root: {
+      width:500,
+      margin:'0 auto',
+      marginTop:'200px'
+    },
+    media: {
+      height: 140,
+    },
+  });
 
 const Entering = (props) =>{
 
+    const classes = useStyles()
+
     return(
-        <div>
-            입국 심사!!
-        </div>
+        <Card className={classes.root}>
+        <CardActionArea>
+      <CardMedia
+        className={classes.media}
+        image="/static/images/entering.jpg"
+        title="Contemplative Reptile"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          Immigration...
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          We will check who you are and where you live stuff like that.
+          For checking your english level, let me test you please.
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+    <CardActions>
+      <Grid container justify="space-evenly">
+          <Grid item>
+              <Button size="small" color="primary">
+                  study
+              </Button>
+          </Grid>
+          <Grid item>
+              <Button size="small" color="primary">
+                  home
+              </Button>
+          </Grid>
+      </Grid>
+    </CardActions>
+  </Card>
     )
 }
 
--- src/LostVillege.js
+++ src/LostVillege.js
@@ -1,14 +1,63 @@
 import React from 'react'
 import Navigation from './Navigation'
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/styles'
+import {Grid} from '@material-ui/core'
+
+const useStyles = makeStyles({
+    root: {
+      width:500,
+      margin:'0 auto',
+      marginTop:'200px'
+    },
+    media: {
+      height: 140,
+    },
+  });
 
 const LostVillege = (props) =>{
 
-    const {history} = props
+    const classes = useStyles();
 
     return (
-        <div>
-            로스트 빌리지!!
-        </div>
+         <Card className={classes.root}>
+          <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="/static/images/village.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lost Village....
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            This is Lost Village. We should repair this villege by studying english..
+            are you ready to study?
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Grid container justify="space-evenly">
+            <Grid item>
+                <Button size="small" color="primary">
+                    study
+                </Button>
+            </Grid>
+            <Grid item>
+                <Button size="small" color="primary">
+                    home
+                </Button>
+            </Grid>
+        </Grid>
+      </CardActions>
+    </Card>
     )
 }
 
