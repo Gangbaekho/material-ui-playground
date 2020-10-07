76a6608decd9f4bcf74d11bbfe5f9f6e631cbc02
Gangbaekho <roorooro9933@gmail.com>
2020-10-07 14:29:29 +0900

    init

---
+++ src/Entering.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navigation from './Navigation'
+
+const Entering = (props) =>{
+
+    return(
+        <>
+            <Navigation {...props}/>
+        </>
+    )
+}
+
+export default Entering
---
+++ src/LostVillege.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navigation from './Navigation'
+
+const LostVillege = (props) =>{
+
+    return (
+    <>
+        <Navigation {...props}/>
+    </>
+    )
+}
+
+export default LostVillege
